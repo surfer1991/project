e8f3fe8b0e0760ed375df35591b2345853460d2b
surfer <surfer.mk1991@gmail.com>
2023-02-13 22:47:48 +0100

    Program.cs

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Ewa";
+int age = 33;
+int age2 = 16;
+bool isFemale = true;
+
+if (!isFemale == true && age == 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age != 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (isFemale = true && age2 < 18)
+{
+    Console.WriteLine("Niepełnoloetni Mężczyzna");
+}
+
+
